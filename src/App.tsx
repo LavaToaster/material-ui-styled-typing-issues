993d09be26c37c6663d3e718ab967b1a0606b251
Adam Lavin <adam.lavin@volo.ai>
2019-09-03 18:21:15 +0100

    Material UI bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { styled } from '@material-ui/core/styles';
+import {
+  compose,
+  palette,
+  sizing,
+  spacing,
+} from '@material-ui/system';
+
+const Example = styled('div')(
+  compose(
+    spacing,
+    sizing,
+    palette,
+  ),
+);
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Example>
+          Test
+        </Example>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
